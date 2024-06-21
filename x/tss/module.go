8c9a4ad0d7e89e999fcb49213a59f1b11a60d341
Van <van@MacBook-Pro-od-Van.local>
2024-06-21 14:14:17 +0800

    tss InitGenesis

--- x/tss/module.go
+++ x/tss/module.go
@@ -107,9 +107,9 @@ func (AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {
 
 // InitGenesis initializes the module's keeper from the given genesis state
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) []abci.ValidatorUpdate {
-	// var genState types.GenesisState
-	// cdc.MustUnmarshalJSON(gs, &genState)
-	// am.keeper.InitGenesis(ctx, &genState)
+	var genState types.GenesisState
+	cdc.MustUnmarshalJSON(gs, &genState)
+	am.keeper.InitGenesis(ctx, &genState)
 
 	return []abci.ValidatorUpdate{}
 }
